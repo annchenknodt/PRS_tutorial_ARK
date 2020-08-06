7386310b52036586c4a109d1566457c4b10243a3
annchenruth <ark19@dcc-slogin-03.oit.duke.edu>
2020-08-06 14:17:47 -0400

    updated run_PRS_PRSice.sh

--- run_PRS_PRSice.sh
+++ run_PRS_PRSice.sh
@@ -1,22 +1,22 @@
-# # Running PRS analysis
-# Rscript format_cov.R
+# Running PRS analysis
+Rscript format_cov.R
 
-# # (ARK added no-regress)
-# Rscript /cifs/hariri-long/Scripts/Tools/PRSice_linux/v2.3.2/PRSice.R \
-    # --prsice /cifs/hariri-long/Scripts/Tools/PRSice_linux/v2.3.2/PRSice_linux \
-    # --no-regress \
-    # --base Height.QC.gz \
-    # --target EUR.QC \
-    # --binary-target F \
-    # --pheno EUR.height \
-    # --cov EUR.cov \
-    # --base-maf MAF:0.01 \
-    # --base-info INFO:0.8 \
-    # --stat OR \
-    # --or \
-    # --out EUR
+# (ARK added no-regress)
+Rscript /cifs/hariri-long/Scripts/Tools/PRSice_linux/v2.3.2/PRSice.R \
+    --prsice /cifs/hariri-long/Scripts/Tools/PRSice_linux/v2.3.2/PRSice_linux \
+    --no-regress \
+    --base Height.QC.gz \
+    --target EUR.QC \
+    --binary-target F \
+    --pheno EUR.height \
+    --cov EUR.cov \
+    --base-maf MAF:0.01 \
+    --base-info INFO:0.8 \
+    --stat OR \
+    --or \
+    --out EUR
 
-### un-recoded 
+### ARK, un-recoded 
 # (ARK added no-regress)
 Rscript /cifs/hariri-long/Scripts/Tools/PRSice_linux/v2.3.2/PRSice.R \
     --prsice /cifs/hariri-long/Scripts/Tools/PRSice_linux/v2.3.2/PRSice_linux \
